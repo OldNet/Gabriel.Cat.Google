ee90acc726a1daaff9792d2a7a3cec5f3e0e594b
tetradog <tetradog@gmail.com>
2016-04-09 23:01:59 +0200

    añadido uso de Gabriel.Cat.Xarxa y Gabriel.Cat.Utilitats y puesta sobrecarga para usar ClienteServidorHttpSeguro

--- GooglePlusLogin.cs
+++ GooglePlusLogin.cs
@@ -80,6 +80,10 @@ namespace Gabriel.Cat.Google
 
         #endregion
         #region Get User login
+        public static GooglePlusUser GetProfile(Gabriel.Cat.Xarxa.ClienteServidorHttpSeguro cliente)
+        {
+            return GetProfile(cliente.Client);
+        }
         public static GooglePlusUser GetProfile(System.Net.HttpListenerContext context) {
             return GetProfile(context.Request.Url);
         }
